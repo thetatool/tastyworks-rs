44f05ca86b52971a975b5d6810763dd998df9c96
Tobias Mansfield-Williams <contact@tmwhere.com>
2024-05-19 14:28:24 +0800

    Fix streamer

--- src/streamer.rs
+++ src/streamer.rs
@@ -13,11 +13,23 @@ const MAX_SUBSCRIPTION_SIZE: usize = 500;
 
 pub struct Client {
     base_url: String,
-    access_token: String,
-    client_id: Option<String>,
+    token: String,
     socket: Option<tungstenite::protocol::WebSocket<tungstenite::client::AutoStream>>,
-    next_message_id: i32,
+    feed_channel: Option<i32>,
     subscription_fields: HashMap<String, Vec<String>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Message {
+    #[serde(rename = "type")]
+    message_type: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ChannelOpenedMessage {
+    #[serde(rename = "type")]
+    message_type: String,
+    channel: i32,
 }
 
 impl Client {
@@ -25,22 +37,21 @@ impl Client {
         #[derive(Debug, Deserialize)]
         #[serde(rename_all = "kebab-case")]
         struct Data {
-            websocket_url: String,
+            dxlink_url: String,
             token: String,
         }
 
-        let response = request("quote-streamer-tokens", "", session).await?;
+        let response = request("api-quote-tokens", "", session).await?;
         let api::Response { data, .. } = response.json::<api::Response<Data>>().await?;
 
-        let base_url = format!("{}/cometd", data.websocket_url).replace("https", "wss");
-        let access_token = data.token;
+        let base_url = data.dxlink_url;
+        let token = data.token;
 
         Ok(Client {
             base_url,
-            access_token,
-            client_id: None,
+            token,
             socket: None,
-            next_message_id: 1,
+            feed_channel: None,
             subscription_fields: HashMap::new(),
         })
     }
@@ -51,89 +62,121 @@ impl Client {
         log::debug!("Connected to dxfeed: {}", response.status());
 
         self.socket = Some(socket);
+        self.send_message(
+            r#"
+{
+  "type": "SETUP",
+  "channel": 0,
+  "keepaliveTimeout": 60,
+  "acceptKeepaliveTimeout": 60,
+  "version": "0.1-js/1.0.0"
+}
+"#,
+        )?;
+        let msg = self.read_message(true)?.ok_or(ReadMessageError)?;
+        let msg_json = msg.to_text()?;
+        let _ = match serde_json::from_str::<Message>(msg_json) {
+            Ok(response) if response.message_type == "SETUP" => response,
+            _ => return Err(ResponseParseError("SETUP".to_string()).into()),
+        };
+        // flush remaining messages e.g. unauthorized auth message
+        while let Some(_) = self.read_message(false)? {}
+
         self.send_message(&format!(
             r#"
-[
-  {{
-    "ext": {{
-      "com.devexperts.auth.AuthToken": "{auth_token}"
-    }},
-    "id": "{id}",
-    "version": "1.0",
-    "minimumVersion": "1.0",
-    "channel": "/meta/handshake",
-    "supportedConnectionTypes": [
-      "websocket",
-      "long-polling",
-      "callback-polling"
-    ],
-    "advice": {{
-      "timeout": 60000,
-      "interval": 0
-    }}
-  }}
-]
+{{
+  "type": "AUTH",
+  "channel": 0,
+  "token": "{}"
+}}
 "#,
-            id = self.next_message_id,
-            auth_token = self.access_token
+            self.token,
         ))?;
-        self.next_message_id += 1;
 
         #[derive(Deserialize)]
-        #[serde(rename_all = "camelCase")]
-        struct DxFeedConnectResponse {
-            client_id: String,
+        struct AuthResponse {
+            state: String,
         }
 
-        let msg = self.read_message(true)?.ok_or(ClientIdResponseError)?;
+        let msg = self.read_message(true)?.ok_or(ReadMessageError)?;
         let msg_json = msg.to_text()?;
-        let client_id = serde_json::from_str::<Vec<DxFeedConnectResponse>>(msg_json)?
-            .into_iter()
-            .next()
-            .ok_or(ClientIdResponseError)?
-            .client_id;
-
-        log::debug!("Parsed dxfeed clientId: {}", client_id);
-        self.client_id = Some(client_id);
-
+        let auth_response = serde_json::from_str::<AuthResponse>(msg_json)
+            .or(Err(ResponseParseError("AUTH".to_string())))?;
+        if auth_response.state != "AUTHORIZED" {
+            return Err(NotAuthorizedError.into());
+        }
         Ok(())
     }
 
     pub fn add_subscription(
         &mut self,
         name: &str,
+        fields: &[String],
         symbols: &[String],
     ) -> Result<(), Box<dyn Error>> {
         if self.socket.is_none() {
             return Err(NotConnectedError.into());
         }
 
+        if self.feed_channel.is_none() {
+            self.send_message(
+                r#"
+{
+  "type": "CHANNEL_REQUEST",
+  "channel": 1,
+  "service": "FEED",
+  "parameters": {
+    "contract": "AUTO"
+  }
+}
+"#,
+            )?;
+            let msg = self.read_message(true)?.ok_or(ReadMessageError)?;
+            let msg_json = msg.to_text()?;
+            let response = match serde_json::from_str::<ChannelOpenedMessage>(msg_json) {
+                Ok(response) if response.message_type == "CHANNEL_OPENED" => response,
+                _ => return Err(ResponseParseError("CHANNEL_OPENED".to_string()).into()),
+            };
+            self.feed_channel = Some(response.channel);
+        }
+
+        if !self.subscription_fields.contains_key(name) {
+            self.send_message(&format!(
+                r#"
+{{
+  "type": "FEED_SETUP",
+  "channel": {channel},
+  "acceptAggregationPeriod": 10,
+  "acceptDataFormat": "COMPACT",
+  "acceptEventFields": {{
+    "{name}": ["{fields}"]
+  }}
+}}
+"#,
+                channel = self.feed_channel.unwrap(),
+                name = name,
+                fields = fields.join("\",\"")
+            ))?;
+            self.subscription_fields
+                .insert(name.to_string(), fields.to_vec());
+        }
+
         for chunk in symbols.chunks(MAX_SUBSCRIPTION_SIZE) {
             self.send_message(&format!(
                 r#"
-[
-  {{
-    "id": "{id}",
-    "channel": "/service/sub",
-    "data": {{
-      "add": {{
-        "{name}": [
-          {symbols}
-        ]
-      }}
-    }},
-    "clientId": "{client_id}"
-  }}
-]
+{{
+  "type": "FEED_SUBSCRIPTION",
+  "channel": {channel},
+  "add": [{add}]
+}}
 "#,
-                id = self.next_message_id,
-                client_id = self.client_id.as_ref().unwrap(),
-                name = name,
-                symbols = chunk.iter().map(|s| format!("\"{}\"", s)).join(",")
+                channel = self.feed_channel.unwrap(),
+                add = chunk
+                    .iter()
+                    .map(|s| format!(r#"{{"type":"{}","symbol":"{}"}}"#, name, s))
+                    .join(",")
             ))?;
-            self.next_message_id += 1;
-
-            // TODO: replace with something more reliable, probably need to handle heartbeat
+            // TODO: replace with something more reliable
             std::thread::sleep(std::time::Duration::from_millis(200));
         }
 
@@ -147,62 +190,56 @@ impl Client {
             return Err(NotConnectedError.into());
         }
 
-        let mut subscription_data = HashMap::new();
+        let mut new_subscription_data = HashMap::new();
         while let Some(msg) = self.read_message(false)? {
             let msg_json = msg.to_text()?;
-            let result = serde_json::from_str::<Vec<DxFeedData>>(msg_json).unwrap_or_default();
-
-            for data in &result {
-                let (name, data_seq) = data.parse_data_seq(&mut self.subscription_fields)?;
-
-                let data = subscription_data
-                    .entry(name.to_string())
-                    .or_insert(SubscriptionData {
-                        subscription_fields: vec![],
-                        data_seq: vec![],
-                    });
-
-                data.data_seq.append(&mut data_seq.clone());
-            }
-        }
-
-        for (name, data) in &mut subscription_data {
-            data.subscription_fields = self
+            let mut feed_data = if let Ok(data) = serde_json::from_str::<DxFeedData>(msg_json) {
+                data
+            } else {
+                continue;
+            };
+            let name = feed_data
+                .data
+                .get(0)
+                .and_then(|name| name.as_str())
+                .map(String::from)
+                .ok_or_else(|| ResponseParseError("name".to_string()))?;
+            let data_seq = feed_data
+                .data
+                .get_mut(1)
+                .and_then(|seq| seq.as_array_mut())
+                .ok_or_else(|| ResponseParseError("data seq".to_string()))?;
+            let subscription_fields = self
                 .subscription_fields
-                .get(name)
-                .ok_or_else(|| ResponseParseError("missing subscription fields".to_string()))?
-                .clone();
+                .get(&name)
+                .ok_or_else(|| ResponseParseError("missing subscription fields".to_string()))?;
+            new_subscription_data
+                .entry(name)
+                .or_insert(SubscriptionData {
+                    subscription_fields: subscription_fields.clone(),
+                    data_seq: vec![],
+                })
+                .data_seq
+                .append(data_seq);
         }
 
-        self.ping()?;
+        self.keep_alive()?;
 
-        Ok(subscription_data)
+        Ok(new_subscription_data)
     }
 
-    fn ping(&mut self) -> Result<(), Box<dyn Error>> {
+    fn keep_alive(&mut self) -> Result<(), Box<dyn Error>> {
         if self.socket.is_none() {
             return Err(NotConnectedError.into());
         }
-
-        self.send_message(&format!(
+        self.send_message(
             r#"
-[
-  {{
-    "id": "{id}",
-    "channel": "/meta/connect",
-    "connectionType": "websocket",
-    "advice": {{
-      "timeout": 0
-    }},
-    "clientId": "{client_id}"
-  }}
-]
+{
+  "type": "KEEPALIVE",
+  "channel": 0
+}
 "#,
-            id = self.next_message_id,
-            client_id = self.client_id.as_ref().unwrap(),
-        ))?;
-        self.next_message_id += 1;
-
+        )?;
         Ok(())
     }
 
@@ -283,85 +320,18 @@ pub struct Price {
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
 struct DxFeedData {
     data: Vec<serde_json::Value>,
 }
 
-impl DxFeedData {
-    fn parse_data_seq(
-        &self,
-        subscription_fields: &mut HashMap<String, Vec<String>>,
-    ) -> Result<(String, &Vec<serde_json::Value>), ResponseParseError> {
-        if self.data.len() != 2 {
-            return Err(ResponseParseError("data length".to_string()));
-        }
-
-        // the first data frame includes both the field names and first data sequence.
-        // subsequent frames contain the remaining data sequences without field names.
-        let has_header = self
-            .data
-            .get(0)
-            .filter(|header| header.is_array())
-            .is_some();
-
-        if has_header {
-            let name = self
-                .data
-                .get(0)
-                .and_then(|header| header.as_array())
-                .and_then(|header| {
-                    match (
-                        header.get(0).and_then(|v| v.as_str()),
-                        header.get(1).and_then(|v| v.as_array()),
-                    ) {
-                        (Some(name), Some(fields)) => {
-                            let mut arr_str = vec![];
-                            for v in fields {
-                                if let Some(s) = v.as_str() {
-                                    arr_str.push(s.to_string());
-                                } else {
-                                    return None;
-                                }
-                            }
-                            subscription_fields.insert(name.to_string(), arr_str);
-                            Some(name)
-                        }
-                        _ => None,
-                    }
-                })
-                .ok_or_else(|| ResponseParseError("header name".to_string()))?;
-            let data_seq = self
-                .data
-                .get(1)
-                .and_then(|seq| seq.as_array())
-                .ok_or_else(|| ResponseParseError("header data seq".to_string()))?;
-
-            Ok((name.to_string(), data_seq))
-        } else {
-            let name = self
-                .data
-                .get(0)
-                .and_then(|name| name.as_str())
-                .ok_or_else(|| ResponseParseError("name".to_string()))?;
-            let data_seq = self
-                .data
-                .get(1)
-                .and_then(|seq| seq.as_array())
-                .ok_or_else(|| ResponseParseError("data seq".to_string()))?;
-            Ok((name.to_string(), data_seq))
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
-struct ClientIdResponseError;
+struct NotAuthorizedError;
 
-impl Error for ClientIdResponseError {}
+impl Error for NotAuthorizedError {}
 
-impl fmt::Display for ClientIdResponseError {
+impl fmt::Display for NotAuthorizedError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Could not find clientId in response")
+        write!(f, "Failed to AUTHORIZE")
     }
 }
 
@@ -373,6 +343,17 @@ impl Error for NotConnectedError {}
 impl fmt::Display for NotConnectedError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "The streamer client is not connected")
+    }
+}
+
+#[derive(Debug, Clone)]
+struct ReadMessageError;
+
+impl Error for ReadMessageError {}
+
+impl fmt::Display for ReadMessageError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed to read message")
     }
 }
 
